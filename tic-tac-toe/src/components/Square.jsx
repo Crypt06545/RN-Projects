d9a9882bcc3df8bdd3523f48e97b8ce97ef0f14c
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-11-02 23:57:55 +0600

    design game board

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 const Square = () => {
   const [value, setValue] = useState(null);
 
-  const handleClick = () => {
-    setValue("X");
-  };
+  // const handleClick = () => {
+  //   setValue("X");
+  // };
   return (
     <>
       <button
