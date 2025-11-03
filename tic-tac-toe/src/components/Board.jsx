4180bf673ba2f4adec7e53e920f0dacf74dad55e
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-11-03 21:44:49 +0600

    implement board x & y logic

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -4,8 +4,12 @@ import Square from "./Square";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsnext, setXIsNext] = useState(true);
+
   const handleClick = (i) => {
     const nexSquares = squares.slice();
+    if (squares[i]) {
+      return;
+    }
     if (xIsnext) {
       nexSquares[i] = "X";
     } else {
@@ -13,13 +17,40 @@ const Board = () => {
     }
     setSquares(nexSquares);
     setXIsNext(!xIsnext);
-    // console.log("clicked", i);
+
+    const calculateWinner = (squares) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+          squares[a] &&
+          squares[a] === squares[b] &&
+          squares[a] === squares[c]
+        ) {
+          return squares[a];
+        }
+      }
+      return null;
+    };
+
+    const winner = calculateWinner(nexSquares);
+    if (winner) {
+      console.log("Winner:", winner);
+    }
   };
 
   return (
     <div className="border-4 border-gray-800 w-64 mx-auto mt-10">
       <div className="grid grid-cols-3 gap-1">
-        {/* 3x3 Grid of Squares */}
         <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
         <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
         <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
